592fa5e47d80d26521dd199c6e91f6289e5e0df2
yang0 <yang0@126.com>
2025-01-12 18:48:04 +0800

    sdfasdf

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # autotask_i18n
 国际化相关的一些功能
+
+
+AutoTask.dev User Id: WMTEf3sDDpvf7CpBAovafJ
--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .index import *
 
-VERSION = "1.0.0"
+VERSION = "0.0.1"
 GIT_URL = "https://github.com/yang0/autotask_i18n"
 NAME = "代码国际化相关功能"
 DESCRIPTION = "代码国际化相关功能"
